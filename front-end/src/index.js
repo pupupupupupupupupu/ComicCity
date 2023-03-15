8238c7582035a9f9c46547e41d164c07c0918f15
pupupupupupupupupu <arsalan.hassan@kalvium.commmunity>
2023-03-15 15:20:01 +0530

    completed my homepage component

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Footer from './components/footer'
-import Navbar from './components/navbar'
+// import Footer from './components/footer'
+// import Navbar from './components/navbar'
+// import HomePage from './components/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <HomePage /> */}
     {/* <Navbar /> */}
     {/* <Footer /> */}
   </React.StrictMode>
